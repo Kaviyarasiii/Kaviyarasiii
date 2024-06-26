b22db4f2e60501bf49c56d8236d4ae44d32fdfda
Goutham <ggowthamsankar@gmail.com>
2024-06-26 15:01:07 +0530

    initial commit

---
+++ src/main/java/in/dev/gmsk/Main.java
@@ -0,0 +1,15 @@
+package in.dev.gmsk;
+
+public class Main {
+
+    /**
+     * Main Method
+     * </p>
+     * System => System is the final class.
+     * out => out is the final static variable initial declaration should be null return type PrintStream
+     * println => is method of PrintStream class, return type void.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
